2f106251be6798044ce4014ba4f34a8c1a7053b7
laderast <roshioshi@gmail.com>
2023-07-27 10:16:47 -0700

    update

--- R/dx.R
+++ R/dx.R
@@ -98,7 +98,7 @@ find_dataset_id <- function(){
 find_all_datasets <- function() {
   iter_py <- dxpy$find_data_objects(
     typename = "Dataset",
-    name="app*.dataset",
+    name="*.dataset",
     folder="/",name_mode="glob",
     recurse = TRUE,
     describe = TRUE)
